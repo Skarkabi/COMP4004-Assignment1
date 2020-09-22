991f6b13f3a701e76a511edd62f304c1b2e16fda
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-22 14:41:52 -0400

    - Adding the Client & Server Directories

---
+++ server/src/main/java/com/Assignment1/server/App.java
@@ -0,0 +1,13 @@
+package com.Assignment1.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
