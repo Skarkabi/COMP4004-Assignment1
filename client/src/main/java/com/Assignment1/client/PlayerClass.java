5947604a20a7b55d2400de9c1fb7b00a2f3d1f16
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-24 15:07:29 -0400

    - Added new properties turn & skullCount to keep track of players turn

--- client/src/main/java/com/Assignment1/client/PlayerClass.java
+++ client/src/main/java/com/Assignment1/client/PlayerClass.java
@@ -2,20 +2,42 @@ package com.Assignment1.client;
 
 import java.util.ArrayList;
 
+//This class is used to represent the player
+//It keeps track of a players dice, score, & takes care of all player interactions
 public class PlayerClass {
-	ArrayList<String> dice = new ArrayList<>();
-	int score = 0;
 	
+	private ArrayList<String> dice = new ArrayList<>(); //ArrayList of Strings to represent to dice
+	private int score = 0; //Used to represent the players score
+	private boolean turn = false;
+	private int skullCount = 0;
+	
+	//This function is used to roll all 8 dice only on the first roll of a players turn
+	//It does this by first making sure the dice ArrayList is cleared, then creating an instance of the dice class, & finally
+	//rolling that instance 8 separate times and records the outcome of each dice in the PlayerClass's dice property
 	public ArrayList<String> firstRoll() {
+		turn = true;
 		dice.clear();
 		DiceClass die = new DiceClass();
-		for(int i = 0; i < 6; i++) {
+		for(int i = 0; i < 8; i++) {
 			dice.add(die.rollDice());
+			if(dice.get(i).equals("Skull")) {
+				skullCount++;
+				
+			}
 
+		} 
+		
+		if(skullCount >= 3) {
+			turn = false; 
+			
 		}
 		
 		return dice;
 	}
+	
+	public ArrayList<String> getDice(){ return dice; }
+	public int getScore() { return score; }
+	public boolean getTurn() { return turn; }
 	
 	
 }
--- client/src/test/java/com/Assignment1/client/DiceTest.java
+++ client/src/test/java/com/Assignment1/client/DiceTest.java
@@ -4,17 +4,19 @@ import java.util.ArrayList;
 
 import junit.framework.TestCase;
 
-public class diceTest extends TestCase {
+public class DiceTest extends TestCase {
 	public void test3Skull() {
 		int skullCount = 0;
 		PlayerClass player = new PlayerClass();
 		ArrayList<String> dice = new ArrayList<>();
 		
 		while (skullCount != 3) {
+			skullCount = 0;
 			dice = player.firstRoll();
 			for(int i = 0; i < dice.size(); i++) {
 				if (dice.get(i).equals("Skull")){
 					skullCount++;
+					System.out.println("Round " + i + " is " + dice.get(i));
 					
 				}
 			}
@@ -22,7 +24,6 @@ public class diceTest extends TestCase {
 		}
 		
 		assertEquals(3, skullCount);
-		
 		
 	}
 
