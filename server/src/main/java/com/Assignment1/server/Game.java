ffdd5c7774139fd1bbea4839b1e7fe3e20efb697
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-29 18:48:48 -0400

    - Added Fortune Card attribute in Game class

--- server/src/main/java/com/Assignment1/server/Game.java
+++ server/src/main/java/com/Assignment1/server/Game.java
@@ -6,7 +6,8 @@ public class Game implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String[] currentRoll = new String[7];
-	private boolean currentTurn = false;
+	private boolean turnOver = false;
+	private String FC = new String("");
 	
 	public int getSymbolCount(String s) {
 		int count = 0;
@@ -20,26 +21,29 @@ public class Game implements Serializable {
 		return count;
 	}
 	
-	public void setCurrentRoll(String[] dice) {
-		currentRoll = dice;
-	}
 	
-	public String[] getOutCome() {
-		return currentRoll;
-	}
+	public void setFortuneCard(String fc) { FC = fc; }
+	
+	public void setCurrentRoll(String[] dice) { currentRoll = dice; }
+	
+	public void setTurn(boolean t) { turnOver = !t; }
+	
+	public String getFortuneCard() { return FC; }
+	 
+	public String[] getOutCome() { return currentRoll; }
 	
 	public boolean isTurnOver() {
 		isDead();
-		return currentTurn;
+		return turnOver;
 	}
 	
 	public boolean isDead() {
 		if(getSymbolCount("Skull") > 2){
-			currentTurn = true;
+			turnOver = true;
 			return true;
 			
 		}else {
-			currentTurn = false;
+			turnOver = false;
 			
 			return false;
 		}
