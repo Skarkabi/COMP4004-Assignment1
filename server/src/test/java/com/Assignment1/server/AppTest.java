82c0c9820fa251a0facb9383d944c5625dd12484
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-29 19:51:24 -0400

    - Added TestCase Corresponding to row 55

--- server/src/test/java/com/Assignment1/server/AppTest.java
+++ server/src/test/java/com/Assignment1/server/AppTest.java
@@ -159,4 +159,20 @@ public class AppTest
     	
     }
     
+    @Test
+    public void testRow55() {
+    	String[] dieRoll = {"Monkey", "Monkey", "Monkey", "Sword", "Sword", "Sword", "Parrot", "Skull"};
+    	PlayerClass p = new PlayerClass("");
+    	p.setGame(game);
+    	game.setCurrentRoll(dieRoll);
+    	game.setFortuneCard("CO");
+    	game.setTurn(false);
+    	assertEquals(3, p.getGame().getSymbolCount("Monkey"));
+    	assertEquals(3, p.getGame().getSymbolCount("Sword"));
+    	assertEquals(300, p.getScore());
+    	assertFalse(p.getGame().isDead());
+    	assertTrue(p.getGame().isTurnOver());
+    	
+    }
+    
 }
