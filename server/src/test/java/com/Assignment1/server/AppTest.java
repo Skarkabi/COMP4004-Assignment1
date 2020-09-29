b1ef5163faf06a4642cc9279f376172b8b64c134
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-29 14:35:23 -0400

    - Added Test Corresponding to Row 51

--- server/src/test/java/com/Assignment1/server/AppTest.java
+++ server/src/test/java/com/Assignment1/server/AppTest.java
@@ -52,4 +52,52 @@ public class AppTest
     	
     }
     
+    @Test
+    public void testRow50() {
+    	String[] dieRoll = {"Skull", "Parrot", "Parrot", "Parrot", "Parrot", "Sword", "Sword", "Skull"};
+    	PlayerClass p = new PlayerClass("");
+    	game.setCurrentRoll(dieRoll);
+    	p.setGame(game);
+    	assertEquals(2, p.getGame().getSymbolCount("Skull"));
+    	assertEquals(4, p.getGame().getSymbolCount("Parrot"));
+    	assertEquals(2, p.getGame().getSymbolCount("Sword"));
+    	
+    	assertEquals(200,p.getScore());
+    	assertFalse(p.getGame().isDead());
+    	
+    	String[] dieRoll2 = {"Skull", "Parrot", "Parrot", "Parrot", "Parrot", "Skull", "Sword", "Skull"};
+    	game.setCurrentRoll(dieRoll2);
+    	assertEquals(3, p.getGame().getSymbolCount("Skull"));
+    	assertEquals(4, p.getGame().getSymbolCount("Parrot"));
+    	assertEquals(1, p.getGame().getSymbolCount("Sword"));
+    	
+    	assertEquals(200, p.getScore());
+    	assertTrue(p.getGame().isDead());
+    	
+    }
+    
+    @Test
+    public void testRow51() {
+    	String[] dieRoll = {"Skull", "Parrot", "Parrot", "Parrot", "Parrot", "Sword", "Sword", "Sword"};
+    	PlayerClass p = new PlayerClass("");
+    	game.setCurrentRoll(dieRoll);
+    	p.setGame(game);
+    	assertEquals(1, p.getGame().getSymbolCount("Skull"));
+    	assertEquals(4, p.getGame().getSymbolCount("Parrot"));
+    	assertEquals(3, p.getGame().getSymbolCount("Sword"));
+    	
+    	assertEquals(300,p.getScore());
+    	assertFalse(p.getGame().isDead());
+    	
+    	String[] dieRoll2 = {"Skull", "Parrot", "Parrot", "Parrot", "Parrot", "Monkey", "Monkey", "Skull"};
+    	game.setCurrentRoll(dieRoll2);
+    	assertEquals(2, p.getGame().getSymbolCount("Skull"));
+    	assertEquals(4, p.getGame().getSymbolCount("Parrot"));
+    	assertEquals(2, p.getGame().getSymbolCount("Monkey"));
+    	
+    	assertEquals(200, p.getScore());
+    	assertTrue(p.getGame().isDead());
+    	
+    }
+    
 }
