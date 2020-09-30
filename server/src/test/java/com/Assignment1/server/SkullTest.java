1a5a90963faa09b816675628fd1eca423ef705ed
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-30 17:46:42 -0400

    - Added TestCase Corresponding to row 114

--- server/src/test/java/com/Assignment1/server/SkullTest.java
+++ server/src/test/java/com/Assignment1/server/SkullTest.java
@@ -136,5 +136,58 @@ public class SkullTest {
     	assertTrue(game.isDead());
     	
     }
+    
+    @Test
+    public void testRow114(){
+    	Game game2 = new Game();
+    	Game game3 = new Game();
+    	
+    	String[] dieRoll = {"Monkey", "Monkey", "Monkey", "Sword", "Sword", "Skull", "Skull", "Skull"};
+    	String[] dieRollAlt = {"Monkey", "Sword", "Parrot", "Sword", "Monkey", "Parrot", "Skull", "Skull"};
+    	
+    	game.setCurrentRoll(dieRoll);
+    	game2.setCurrentRoll(dieRollAlt);
+    	game3.setCurrentRoll(dieRollAlt);
+    	
+    	PlayerClass p = new PlayerClass("");
+    	PlayerClass p2 = new PlayerClass("");
+    	PlayerClass p3 = new PlayerClass("");
+    	p.setGame(game);
+    	p2.setGame(game2);
+    	p3.setGame(game3);
+    	game.setFortuneCard("SK1");
+    	
+    	assertEquals(400, p.getDeductionSent());
+    	assertEquals(0, p.getScore());
+    	assertFalse(p.getGame().isTurnOver());
+    	assertFalse(game.isDead());
+    	
+    	String[] dieRoll2 = {"Skull", "Monkey", "Monkey", "Sword", "Sword", "Skull", "Skull", "Skull"};
+    	game.setCurrentRoll(dieRoll);
+    	
+    	assertEquals(500, p.getDeductionSent());
+    	assertEquals(0, p.getScore());
+    	assertFalse(p.getGame().isTurnOver());
+    	assertFalse(game.isDead());
+    	
+    	String[]dieRoll3 = {"Skull", "Parrot", "Monkey", "Sword", "Sword", "Skull", "Skull", "Skull"};
+    	game.setCurrentRoll(dieRoll);
+    	game.setTurn(false);
+    	
+    	p2.setScore(1000);
+    	p3.setScore(2000);
+    	
+    	
+    	p2.setDeductionReieved(p.getDeductionSent());
+    	p3.setDeductionReieved(p.getDeductionSent());
+    	
+    	assertEquals(500, p2.getScore());
+    	assertEquals(1500, p3.getScore());
+    	assertEquals(500, p.getDeductionSent());
+    	assertEquals(0, p.getScore());
+    	assertTrue(p.getGame().isTurnOver());
+    	assertTrue(game.isDead());
+    	
+    }
 
 }
