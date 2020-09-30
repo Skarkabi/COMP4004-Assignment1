1d0ad89a9e0977fd5ef9eb42a44ca9fa3e2c99ee
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-30 15:46:40 -0400

    - Added TestCase Corresponding to row 101

---
+++ server/src/test/java/com/Assignment1/server/FullChestTest.java
@@ -0,0 +1,29 @@
+package com.Assignment1.server;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FullChestTest {
+	Game game = new Game();
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testRow101(){
+    	String[] dieRoll = {"Monkey", "Monkey", "Monkey", "Sword", "Sword", "Sword", "Diamond", "Parrot"};
+    	game.setCurrentRoll(dieRoll);
+    	PlayerClass p = new PlayerClass("");
+    	p.setGame(game);
+    	game.setTurn(false);
+    	game.setFortuneCard("CO");
+    	
+    	assertEquals(400, p.getScore());
+    	assertTrue(p.getGame().isTurnOver());
+    	assertFalse(game.isDead());
+    	
+    }
+
+}
