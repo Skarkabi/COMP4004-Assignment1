9c46cf4e1260ab75e8be0625f72af9442fcc1789
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-30 17:17:08 -0400

    - Added TestCase Corresponding to row 110

---
+++ server/src/test/java/com/Assignment1/server/SkullTest.java
@@ -0,0 +1,29 @@
+package com.Assignment1.server;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SkullTest {
+	Game game = new Game();
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testRow110(){
+    	String[] dieRoll = {"Monkey", "Monkey", "Monkey", "Sword", "Sword", "Sword", "Diamond", "Skull"};
+    	game.setCurrentRoll(dieRoll);
+    	PlayerClass p = new PlayerClass("");
+    	p.setGame(game);
+    	game.setTurn(false);
+    	game.setFortuneCard("SK2");
+    	
+    	assertEquals(0, p.getScore());
+    	assertTrue(p.getGame().isTurnOver());
+    	assertTrue(game.isDead());
+    	
+    }
+
+}
