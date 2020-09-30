ac7bf7dcd2e1b64404507332897ec120ea1a2d7f
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-30 17:43:46 -0400

    - Added TestCase Corresponding row 113

--- server/src/test/java/com/Assignment1/server/SkullTest.java
+++ server/src/test/java/com/Assignment1/server/SkullTest.java
@@ -87,8 +87,53 @@ public class SkullTest {
     	assertEquals(1300, p3.getScore());
     	assertEquals(700, p.getDeductionSent());
     	assertEquals(0, p.getScore());
+    	assertTrue(p.getGame().isTurnOver());
+    	assertTrue(game.isDead());
+    	
+    }
+    
+    @Test
+    public void testRow113(){
+    	Game game2 = new Game();
+    	Game game3 = new Game();
+    	
+    	String[] dieRoll = {"Monkey", "Monkey", "Monkey", "Sword", "Sword", "Skull", "Skull", "Skull"};
+    	String[] dieRollAlt = {"Monkey", "Sword", "Parrot", "Sword", "Monkey", "Parrot", "Skull", "Skull"};
+    	
+    	game.setCurrentRoll(dieRoll);
+    	game2.setCurrentRoll(dieRollAlt);
+    	game3.setCurrentRoll(dieRollAlt);
+    	
+    	PlayerClass p = new PlayerClass("");
+    	PlayerClass p2 = new PlayerClass("");
+    	PlayerClass p3 = new PlayerClass("");
+    	p.setGame(game);
+    	p2.setGame(game2);
+    	p3.setGame(game3);
+    	game.setFortuneCard("SK2");
+    	
+    	assertEquals(500, p.getDeductionSent());
+    	assertEquals(0, p.getScore());
     	assertFalse(p.getGame().isTurnOver());
     	assertFalse(game.isDead());
+    	
+    	String[] dieRoll2 = {"Sword", "Sword", "Parrot", "Monkey", "Monkey", "Skull", "Skull", "Skull"};
+    	game.setCurrentRoll(dieRoll);
+    	
+    	
+    	p2.setScore(1000);
+    	p3.setScore(2000);
+    	
+    	
+    	p2.setDeductionReieved(p.getDeductionSent());
+    	p3.setDeductionReieved(p.getDeductionSent());
+    	
+    	assertEquals(500, p2.getScore());
+    	assertEquals(1500, p3.getScore());
+    	assertEquals(500, p.getDeductionSent());
+    	assertEquals(0, p.getScore());
+    	assertTrue(p.getGame().isTurnOver());
+    	assertTrue(game.isDead());
     	
     }
 
