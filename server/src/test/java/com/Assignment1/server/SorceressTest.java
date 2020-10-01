166dc831a3ee6c4130c5be48352c56bcf4e5166a
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-30 21:39:41 -0400

    - Added TestCase Corresponding to row 80

---
+++ server/src/test/java/com/Assignment1/server/SorceressTest.java
@@ -0,0 +1,33 @@
+package com.Assignment1.server;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SorceressTest {
+	Game game = new Game();
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testRow80(){
+    	String[] dieRoll = {"Monkey", "Monkey", "Monkey", "Sword", "Sword", "Sword", "Diamond", "Skull"};
+    	int[] dieToReRoll = {8};
+    	game.setCurrentRoll(dieRoll);
+    	PlayerClass p = new PlayerClass("");
+    	p.setGame(game);
+    	game.setFortuneCard("SO");
+    	game.setTurn(false);
+    	String[] originalRoll = game.getOutCome();
+    	game.reRoll(dieToReRoll);
+    	
+    	assertTrue(originalRoll != game.getOutCome());
+    	assertEquals(0, game.getSymbolCount("Skull"));
+    	assertTrue(p.getGame().isTurnOver());
+    	assertFalse(game.isDead());
+    	
+    }
+    
+}
