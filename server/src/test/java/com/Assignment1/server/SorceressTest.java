618a87152f0b2f9ce4741cab041be45f35bbba5f
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2020-09-30 21:48:51 -0400

    - Added TestCase Corresponding to row 81

--- server/src/test/java/com/Assignment1/server/SorceressTest.java
+++ server/src/test/java/com/Assignment1/server/SorceressTest.java
@@ -20,11 +20,43 @@ public class SorceressTest {
     	p.setGame(game);
     	game.setFortuneCard("SO");
     	String[] originalRoll = game.getOutCome();
+    	assertEquals(1, game.getSymbolCount("Skull"));
     	game.reRoll(dieToReRoll);
     	
     	assertTrue(originalRoll != game.getOutCome());
     	assertEquals(0, game.getSymbolCount("Skull"));
     	assertFalse(p.getGame().isTurnOver());
+    	assertFalse(game.isDead());
+    	
+    }
+    
+    @Test
+    public void testRow81(){
+    	String[] dieRoll = {"Monkey", "Monkey", "Monkey", "Sword", "Sword", "Sword", "Diamond", "Sword"};
+    	game.setCurrentRoll(dieRoll);
+    	PlayerClass p = new PlayerClass("");
+    	p.setGame(game);
+    	game.setCurrentRoll(dieRoll);
+    	game.setFortuneCard("SO");
+    	
+    	assertFalse(p.getGame().isTurnOver());
+    	assertFalse(game.isDead());
+    	
+    	String[] dieRoll2 = {"Skull", "Monkey", "Monkey", "Sword", "Sword", "Sword", "Diamond", "Sword"};
+    	int[] dieToReRoll = {1};
+    	game.setCurrentRoll(dieRoll2);
+    	String[] originalRoll = game.getOutCome();
+
+    	assertEquals(1, game.getSymbolCount("Skull"));
+    	assertFalse(p.getGame().isTurnOver());
+    	assertFalse(game.isDead());
+    	
+    	game.reRoll(dieToReRoll);
+    	game.setTurn(false);
+    	assertTrue(originalRoll != game.getOutCome());
+    	assertEquals(0, game.getSymbolCount("Skull"));
+    	assertEquals(200, p.getScore());
+    	assertTrue(p.getGame().isTurnOver());
     	assertFalse(game.isDead());
     	
     }
